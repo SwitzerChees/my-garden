3e42b134a98775dd9a9d95dfca60643914c325b9
switzerchees <patrickmichel96@yahoo.de>
2023-01-22 17:35:32 +0100

    Add upload image functionality

--- pages/my-plants/[id].vue
+++ pages/my-plants/[id].vue
@@ -3,6 +3,38 @@
     <div class="w-112">
       <div class="flex flex-col gap-6">
         <PlantDetails v-if="plant" :plant="plant" @watered="fetchPlant" @fertilized="fetchPlant" />
+        <div class="flex justify-between">
+          <Button class="p-button-text">
+            <div class="flex items-center gap-1">
+              <Icon name="fontisto:scissors" size="1.2rem" />
+              <span>Brune</span>
+            </div>
+          </Button>
+          <div class="flex flex-col">
+            <Button class="p-button-success" @click="startUpload">
+              <div class="flex items-center gap-1">
+                <Icon name="material-symbols:photo-camera" size="1.2rem" />
+              </div>
+            </Button>
+            <ProgressBar v-if="progressUpload" mode="indeterminate" style="height: 0.3rem" />
+          </div>
+          <div class="hidden">
+            <FileUpload
+              ref="upload"
+              name="photo"
+              url="/api/upload"
+              :auto="true"
+              accept="image/*"
+              @upload="uploadComplete"
+              @before-send="progressUpload = true" />
+          </div>
+          <Button class="p-button-text">
+            <div class="flex items-center gap-1">
+              <Icon name="material-symbols:potted-plant-outline" size="1.2rem" />
+              <span>Repot</span>
+            </div>
+          </Button>
+        </div>
         <PlantHistory v-if="plant" class="ml-4" :history="orderedHistory" />
       </div>
     </div>
@@ -10,9 +42,12 @@
 </template>
 
 <script setup lang="ts">
-  import { Plant } from '~~/definitions'
+  import lfp from 'lodash/fp'
+  import { Photo, Plant } from '~~/definitions'
+  import { addHistoryElement } from '~~/surrealdb/mutations'
   import { getPlant } from '~~/surrealdb/queries'
   let plant = $ref<Plant>()
+  const { first } = lfp
 
   const route = useRoute()
   const fetchPlant = async () => {
@@ -25,6 +60,22 @@
     if (!plant) return []
     return plant.history.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
   })
+
+  let progressUpload = $ref(false)
+
+  const uploadComplete = async ({ xhr: { response: photos } }: { xhr: { response: string } }) => {
+    progressUpload = false
+    const photo = first(JSON.parse(photos)) as Photo
+    if (!photo || !plant?.id) return
+    const historyElement = await addHistoryElement(plant.id, { action: 'image', photo, createdAt: new Date() })
+    if (!historyElement) return
+    plant.history.push(historyElement)
+  }
+  const upload = $ref<any>(null)
+  const startUpload = () => {
+    if (!upload) return
+    upload.choose()
+  }
 
   onMounted(fetchPlant)
 
