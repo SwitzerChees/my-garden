99a215bb422465015ceed48b6bc85a85598a056f
switzerchees <patrickmichel96@yahoo.de>
2023-01-22 23:30:39 +0100

    Use surrealdb.hackerman.ch

--- surrealdb/index.ts
+++ surrealdb/index.ts
@@ -1,5 +1,6 @@
 import Surreal from 'surrealdb.js'
-export const db = new Surreal('http://192.168.1.115:8000/rpc')
+// export const db = new Surreal('http://192.168.1.115:8000/rpc')
+export const db = new Surreal('https://surrealdb.hackerman.ch/rpc')
 
 await db.signin({
   user: 'root',
