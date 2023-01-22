d08c8ca15a683d08b951df382ccd51d71332e4fe
switzerchees <patrickmichel96@yahoo.de>
2023-01-22 12:33:26 +0100

    Fix card history when directly apply water or fertilize

--- pages/my-plants/index.vue
+++ pages/my-plants/index.vue
@@ -2,14 +2,23 @@
   <div class="flex flex-col gap-2">
     <div></div>
     <div class="flex flex-col gap-6 grow md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <PlantCard v-for="plant of plants" :key="plant.id" :plant="plant" />
+      <PlantCard
+        v-for="plant of plants"
+        :key="plant.id"
+        :plant="plant"
+        @watered="addHistory(plant, $event)"
+        @fertilized="addHistory(plant, $event)" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { Plant } from '~~/definitions'
+  import { HistoryElement, Plant } from '~~/definitions'
   import { getPlants } from '~~/surrealdb/queries'
+
+  const addHistory = (plant: Plant, historyElement: HistoryElement) => {
+    plant.history.push(historyElement)
+  }
 
   let plants = $ref<Plant[]>([])
   onMounted(async () => {
