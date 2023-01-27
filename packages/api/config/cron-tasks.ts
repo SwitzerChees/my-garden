c5b88e7eea4b2cf2d9599c0d2389e6e181570602
switzerchees <patrickmichel96@yahoo.de>
2023-01-27 08:25:18 +0100

    Fix crontab

--- packages/api/config/cron-tasks.ts
+++ packages/api/config/cron-tasks.ts
@@ -5,7 +5,7 @@ export default {
   // Every minute
   // '* * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
   // Every day at 20:00 send reminders
-  '*/15 * * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
+  '0 18 * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
   // Every day at 09:00 send reminders
   '0 9 * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
 }
