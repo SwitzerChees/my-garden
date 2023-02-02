611d4c0a52d62049c106df0c7cab1bb9ddc084ce
switzerchees <patrickmichel96@yahoo.de>
2023-02-02 21:56:50 +0100

    Fix group key header

--- packages/api/src/utils/reminder.ts
+++ packages/api/src/utils/reminder.ts
@@ -1,6 +1,6 @@
 import lfp from 'lodash/fp'
 import { HistoryElement, Plant, ReminderSummary, ReminderSummaryEntry } from '@my-garden/common/definitions'
-import { dateToday, dayInMilliseconds, daysBetweenDates, isoDateWithoutTime, relativeDate } from '.'
+import { dateToday, dayInMilliseconds, daysBetweenDates, isoDateWithoutTime, isValidDate, relativeDate } from '.'
 const { first, orderBy, pipe, uniqBy } = lfp
 
 const waterActionTypes = ['watered']
@@ -111,7 +111,7 @@ export const getPlantsGroupedByReminder = (plants: Plant[]) => {
     plantGroupArray.splice(plantGroupArray.indexOf(todayReminder), 1)
     plantGroupArray.unshift(todayReminder)
   }
-  return plantGroupArray.map((p) => ({ key: relativeDate(p.key), plants: p.plants }))
+  return plantGroupArray.map((p) => ({ key: isValidDate(p.key) ? relativeDate(p.key) : p.key, plants: p.plants }))
 }
 
 export const getPlantsToday = (plants: Plant[]) => {
