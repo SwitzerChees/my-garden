2ef46ee4f1e3d211bd84a612a93ecbc72d234373
switzerchees <patrickmichel96@yahoo.de>
2023-02-03 23:06:31 +0100

    Remove every minute crontab

--- packages/api/config/cron-tasks.ts
+++ packages/api/config/cron-tasks.ts
@@ -3,7 +3,7 @@ import { sendReminders } from '../src/reminder/telegram'
 
 export default {
   // Every minute
-  '* * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
+  // '* * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
   // Every day at 20:00 send reminders
   '0 18 * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
   // Every day at 09:00 send reminders
