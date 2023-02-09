32c7fe4c097346458e59c41cc94bb2b3c5d2592c
switzerchees <patrickmichel96@yahoo.de>
2023-02-09 13:40:24 +0100

    Auto grow to bring footer to the  end

--- packages/ui/layouts/default.vue
+++ packages/ui/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col min-w-full min-h-screen text-gray-100 bg-slate-800 pb-14 md:pb-0">
     <HeaderBar class="fixed top-0 left-0 right-0 z-40 p-2 hidden md:flex" />
     <HeaderSidebar class="fixed top-0 bottom-0 left-0 z-50 hidden md:flex" />
-    <div class="p-6 pt-4 md:pt-24 md:pl-28">
+    <div class="p-6 pt-4 md:pt-24 md:pl-28 grow">
       <slot />
     </div>
     <FooterBar v-if="isLoggedIn" class="md:hidden fixed -bottom-0.5 left-0 right-0 z-50 p-2" />
