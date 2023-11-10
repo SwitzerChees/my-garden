0e3fd000c39b3ae6640fa3ecbcb3f68ec69ed097
switzerchees <patrickmichel96@yahoo.de>
2023-11-10 18:25:31 +0100

    Remove all related with telegram push notifications replaced now with web push

--- packages/api/config/cron-tasks.ts
+++ packages/api/config/cron-tasks.ts
@@ -1,14 +1,11 @@
 import { Strapi } from '@strapi/strapi'
-import { sendReminders } from '../src/reminder/telegram'
 import { sendPushReminders } from '../src/reminder'
 
 export default {
   // Every minute
   // '* * * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
   // Every day at 20:00 send reminders
-  '0 18 * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
+  '0 18 * * *': async ({ strapi }: { strapi: Strapi }) => await sendPushReminders(strapi),
   // Every day at 09:00 send reminders
-  '0 9 * * *': async ({ strapi }: { strapi: Strapi }) => await sendReminders(strapi),
-  // Every minute send push reminders
-  '* * * * *': async ({ strapi }: { strapi: Strapi }) => await sendPushReminders(strapi),
+  '0 9 * * *': async ({ strapi }: { strapi: Strapi }) => await sendPushReminders(strapi),
 }
--- packages/common/definitions/collection-types/TelegramConfig.ts
+++
@@ -1,6 +0,0 @@
-import { StrapiObject } from '.'
-
-export interface TelegramConfig extends StrapiObject {
-  apiToken: string
-  chatId: string
-}
